68f12f578e5e10888853cb4a8d54c8d7b198a262
edoardomachado <edoardomachado@gmail.com>
2024-07-11 13:54:35 -0400

    Test for method not allowed was written

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -188,5 +188,8 @@ class TestAccountService(TestCase):
         # assert that the len() of the data is 5 (the number of accounts you created)        
         self.assertEqual(len(data), 5)
 
-
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
